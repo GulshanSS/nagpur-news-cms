81d3c0e8ac7c1855ed43d4d492c7dbb3d39d13ae
GulshanSS <sahugulshan686@gmail.com>
2023-06-20 20:58:42 +0530

    Fix: minor changes w.r.t custom error handling && using async handler in controllers

--- server/src/controller/category.controller.ts
+++ server/src/controller/category.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
-import logger from "../utils/logger";
+import { NextFunction, Request, Response } from "express";
+import asyncHandler from "../middleware/asyncHandler";
+import { AppError, HttpCode } from "../exceptions/AppError";
 import {
   createCategory,
   deleteCategoryById,
@@ -14,115 +15,108 @@ import {
   UpdateCategoryInput,
 } from "../schemas/category.schema";
 
-export const getAllCategoriesHandler = async (req: Request, res: Response) => {
-  try {
+export const getAllCategoriesHandler = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     const categories = await getAllCategories();
     if (categories.length === 0) {
-      return res.status(200).json({
-        statusCode: 200,
-        success: false,
-        message: "No Categories Found",
-      });
+      next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: "No Categories Found",
+        })
+      );
     }
-    return res.json(200).json({
-      statusCode: 200,
+    return res.status(HttpCode.OK).json({
       success: true,
       count: categories.length,
       categories,
     });
-  } catch (e: unknown) {
-    if (e instanceof Error) logger.error(e.message);
   }
-};
+);
 
-export const getCategoryByIdHandler = async (
-  req: Request<GetCategoryInput["params"], {}, {}>,
-  res: Response
-) => {
-  try {
+export const getCategoryByIdHandler = asyncHandler(
+  async (
+    req: Request<GetCategoryInput["params"], {}, {}>,
+    res: Response,
+    next: NextFunction
+  ) => {
     const categoryId = req.params.categoryId;
     const category = await getCategoryById(categoryId);
     if (!category) {
-      return res.status(404).json({
-        statusCode: 404,
-        success: false,
-        message: `Category not found with id ${categoryId}`,
-      });
+      next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: `Category not found with id ${categoryId}`,
+        })
+      );
     }
-    return res.status(200).json({
-      statusCode: 200,
+    return res.status(HttpCode.OK).json({
       success: true,
       category,
     });
-  } catch (e: unknown) {
-    if (e instanceof Error) logger.error(e.message);
   }
-};
+);
 
-export const createCategoryHandler = async (
-  req: Request<{}, {}, CreateCategoryInput["body"]>,
-  res: Response
-) => {
-  try {
+export const createCategoryHandler = asyncHandler(
+  async (req: Request<{}, {}, CreateCategoryInput["body"]>, res: Response) => {
     const data = req.body;
     const category = await createCategory({ ...data });
-    return res.status(201).json({
-      statusCode: 201,
+    return res.status(HttpCode.CREATED).json({
       success: true,
       category,
     });
-  } catch (e: unknown) {
-    if (e instanceof Error) logger.error(e.message);
   }
-};
+);
 
-export const updateCategoryByIdHandler = async (
-  req: Request<UpdateCategoryInput["params"], {}, UpdateCategoryInput["body"]>,
-  res: Response
-) => {
-  try {
+export const updateCategoryByIdHandler = asyncHandler(
+  async (
+    req: Request<
+      UpdateCategoryInput["params"],
+      {},
+      UpdateCategoryInput["body"]
+    >,
+    res: Response,
+    next: NextFunction
+  ) => {
     const categoryId = req.params.categoryId;
     const oldCategory = await getCategoryById(categoryId);
     if (!oldCategory) {
-      return res.status(404).json({
-        statusCode: 404,
-        success: false,
-        message: `Category not found with id ${categoryId}`,
-      });
+      next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: `Category not found with id ${categoryId}`,
+        })
+      );
     }
     const data = req.body;
     const updatedCategory = await updateCategoryById(categoryId, { ...data });
-    return res.status(201).json({
-      statusCode: 201,
+    return res.status(HttpCode.CREATED).json({
       success: true,
       updatedCategory,
     });
-  } catch (e: unknown) {
-    if (e instanceof Error) logger.error(e.message);
   }
-};
+);
 
-export const deleteCategoryByIdHandler = async (
-  req: Request<DeleteCategoryInput["params"], {}, {}>,
-  res: Response
-) => {
-  try {
+export const deleteCategoryByIdHandler = asyncHandler(
+  async (
+    req: Request<DeleteCategoryInput["params"], {}, {}>,
+    res: Response,
+    next: NextFunction
+  ) => {
     const categoryId = req.params.categoryId;
     const category = await getCategoryById(categoryId);
     if (!category) {
-      return res.status(404).json({
-        statusCode: 404,
-        success: false,
-        message: `Category not found with id ${categoryId}`,
-      });
+      next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: `Category not found with id ${categoryId}`,
+        })
+      );
     }
     await deleteCategoryById(categoryId);
-    return res.status(204).json({
-      statusCode: 204,
+    return res.status(HttpCode.OK).json({
       success: true,
       message: `Category with id ${categoryId} deleted successfully`,
     });
-  } catch (e: unknown) {
-    if (e instanceof Error) logger.error(e.message);
   }
-};
+);
--- server/src/middleware/validateSchema.ts
+++ server/src/middleware/validateSchema.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AnyZodObject, ZodError, ZodIssue } from "zod";
+import { AppError, HttpCode } from "../exceptions/AppError";
 
 const validateSchema =
   (schema: AnyZodObject) =>
@@ -17,18 +18,22 @@ const validateSchema =
         e.errors?.map(
           (err: ZodIssue) => (validationErrors[err.path[1]] = err.message)
         );
-        return res.status(400).json({
-          statusCode: 400,
-          success: false,
-          errors: validationErrors,
-        });
+        return next(
+          new AppError({
+            name: "Validation Errors",
+            httpCode: HttpCode.BAD_REQUEST,
+            description: "Error validating resource",
+            errors: validationErrors,
+          })
+        );
       }
       if (e instanceof Error) {
-        return res.status(409).json({
-          statusCode: 400,
-          success: false,
-          message: e.message,
-        });
+        return next(
+          new AppError({
+            httpCode: HttpCode.BAD_REQUEST,
+            description: e.message,
+          })
+        );
       }
     }
   };
