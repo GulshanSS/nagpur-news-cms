580e82571a217980ea436cc29d14ae64e5d38d0e
GulshanSS <sahugulshan686@gmail.com>
2023-06-22 00:12:56 +0530

    Add: cookie-parser and cors middleware && register auth router to server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,14 +6,26 @@ import config from "./config";
 import logger from "./utils/logger";
 import CategoryRouter from "./routes/category.routes";
 import errorMiddlerware from "./middleware/errorMiddleware";
+import AuthRouter from "./routes/auth.routes";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
 
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 app.use(express.json());
 
 app.get("/test", (_: Request, res: Response) => {
   res.send("Nagpur News API");
 });
 
+app.use("/api/v1", AuthRouter);
 app.use("/api/v1/category", CategoryRouter);
 
 app.use(errorMiddlerware);
