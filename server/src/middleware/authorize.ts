64c4ddb48dda8d4b7bff158030e188bfc6b7f83e
GulshanSS <sahugulshan686@gmail.com>
2023-06-22 00:10:58 +0530

    Add: authorization middleware

---
+++ server/src/middleware/authorize.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError, HttpCode } from "../exceptions/AppError";
+
+export const authorize = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(res.locals.payload.role)) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.UNAUTHORIZED,
+          description: `Role: ${res.locals.payload.role} is not allowed to access this resource`,
+        })
+      );
+    }
+  };
+};
