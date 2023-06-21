7bdcde340ae068a4ba21ef079689c828276212c4
GulshanSS <sahugulshan686@gmail.com>
2023-06-22 00:04:06 +0530

    Add: JWT required methods to fenerate tokens

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import { User } from "@prisma/client";
+
+import config from "../config";
+
+export interface ITokenPayload {
+  userId: string;
+  role: string;
+  jti?: string;
+  iat: number;
+  exp: number;
+}
+
+export const generateAccessToken = (user: User): string => {
+  return jwt.sign({ userId: user.id }, config.JWT_ACCESS_TOKEN_SECRET, {
+    expiresIn: config.JWT_ACCESS_TOKEN_EXPIRE_TIME,
+  });
+};
+
+export const generateRefreshToken = (user: User, jti: string): string => {
+  return jwt.sign({ userId: user.id, jti }, config.JWT_REFRESH_TOKEN_SECRET, {
+    expiresIn: config.JWT_REFRESH_TOKEN_EXPIRE_TIME,
+  });
+};
+
+export const generateTokens = (
+  user: User,
+  jti: string
+): {
+  accessToken: string;
+  refreshToken: string;
+} => {
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user, jti);
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
+
+export const verifyToken = (refreshToken: string, jwtSecret: string) =>
+  jwt.verify(refreshToken, jwtSecret) as ITokenPayload;
