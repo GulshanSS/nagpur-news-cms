a02f3aa0a729011c787c8d54cf9f1e2e7b4516f6
GulshanSS <sahugulshan686@gmail.com>
2023-06-22 14:07:09 +0530

    Add: otp routes

---
+++ server/src/routes/otp.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { sendOtpHandler, verifyOtpHandler } from "../controller/otp.controller";
+import validateSchema from "../middleware/validateSchema";
+import { sendOtpSchema, verifyOtpSchema } from "../schemas/otp.schema";
+
+const OtpRouter = Router();
+
+OtpRouter.get("/send-otp", validateSchema(sendOtpSchema), sendOtpHandler);
+OtpRouter.post(
+  "/verify-otp",
+  validateSchema(verifyOtpSchema),
+  verifyOtpHandler
+);
+
+export default OtpRouter;
