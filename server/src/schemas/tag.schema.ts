a9aa7a4fe22d6bca702738ddf52db74a71e6aeef
GulshanSS <sahugulshan686@gmail.com>
2023-06-23 12:55:11 +0530

    Add: Tag validation schema

---
+++ server/src/schemas/tag.schema.ts
@@ -0,0 +1,24 @@
+import { TypeOf, boolean, object, string } from "zod";
+
+const payload = {
+  body: object({
+    name: string().min(1, "Tag Name is required"),
+    active: boolean().optional(),
+  }),
+};
+
+const params = {
+  params: object({
+    tagId: string().min(1, "TagId is required"),
+  }),
+};
+
+export const createTagSchema = object({ ...payload });
+export const updateTagSchema = object({ ...params, ...payload });
+export const getTagSchema = object({ ...params });
+export const deleteTagSchema = object({ ...params });
+
+export type CreateTagInput = TypeOf<typeof createTagSchema>;
+export type UpdateTagInput = TypeOf<typeof updateTagSchema>;
+export type GetTagInput = TypeOf<typeof getTagSchema>;
+export type DeleteTagInput = TypeOf<typeof deleteTagSchema>;
