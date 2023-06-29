54f64347aa15b86af336d1bdfd5455ef83548f46
GulshanSS <sahugulshan686@gmail.com>
2023-06-29 23:40:16 +0530

    Add: controlled custom input field component using react-hook-form

--- client/src/components/FormComponents/InputField.tsx
+++ client/src/components/FormComponents/InputField.tsx
@@ -1,3 +1,5 @@
+import { Controller, useFormContext } from "react-hook-form";
+
 const InputField = ({
   label,
   name,
@@ -9,22 +11,41 @@ const InputField = ({
   type: string;
   placeholder: string;
 }) => {
+  const { control } = useFormContext();
+
   return (
     <>
-      <div className="mb-6">
-        <label
-          htmlFor={name}
-          className="block mb-2 text-sm font-semibold text-gray-900"
-        >
-          {label}
-        </label>
-        <input
-          type={type}
-          id={name}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-none"
-          placeholder={placeholder}
-        />
-      </div>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue=""
+        render={({ field, fieldState }) => (
+          <>
+            <div className="mb-6">
+              <label
+                htmlFor={name}
+                className="block mb-2 text-sm font-semibold text-gray-900"
+              >
+                {label}
+              </label>
+              <input
+                {...field}
+                type={type}
+                id={name}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-none"
+                placeholder={placeholder}
+              />
+              {fieldState.error ? (
+                <span className="flex justify-end font-bold text-[10px] text-red-600 mr-2">
+                  {fieldState.error.message}
+                </span>
+              ) : (
+                ""
+              )}
+            </div>
+          </>
+        )}
+      />
     </>
   );
 };
