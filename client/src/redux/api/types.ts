c67ebeeb5be85de6d1529ea372b4759bb2affe28
GulshanSS <sahugulshan686@gmail.com>
2023-07-07 19:35:34 +0530

    Add: Required Types

--- client/src/redux/api/types.ts
+++ client/src/redux/api/types.ts
@@ -1,5 +1,33 @@
-export interface IUser {
+export type OTPInput = {
+  otp: string;
+  userId: string;
+};
+
+export type User = {
   id: number;
   email: string;
   verified: boolean;
-}
+};
+
+export type Category = {
+  id: number;
+  name: string;
+  active: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type APIErrorResponse = {
+  data: {
+    success: boolean;
+    message: string;
+  };
+};
+
+export type OTPErrorResponse = {
+  data: {
+    success: boolean;
+    userId: string;
+    message: string;
+  };
+};
