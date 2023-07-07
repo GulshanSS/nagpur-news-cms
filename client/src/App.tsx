223130291c0548355851ea8a346cb8969fe81712
GulshanSS <sahugulshan686@gmail.com>
2023-07-07 19:27:38 +0530

    Add: Toast Container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import OTPPage from "./pages/OTPPage";
+import CategoryPage from "./pages/CategoryPage";
+import TagPage from "./pages/TagPage";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -11,7 +15,23 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/verify-otp" element={<OTPPage />} />
         </Routes>
+        <Routes>
+          <Route path="/category" element={<CategoryPage />} />
+          <Route path="/tag" element={<TagPage />} />
+        </Routes>
       </BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
