3c54335566a560f5b0cc26bf0e6a120a87fd5bbb
GulshanSS <sahugulshan686@gmail.com>
2023-07-07 19:28:05 +0530

    Add: Action Button Component

---
+++ client/src/components/ActionButton.tsx
@@ -0,0 +1,28 @@
+import { MouseEventHandler } from "react";
+
+type Props = {
+  onClick?: MouseEventHandler;
+  Icon: React.ReactNode;
+  color: string;
+};
+
+const ActionButton = ({ onClick, Icon, color }: Props) => {
+  const colorVariants: any = {
+    blue: "text-blue-500",
+    red: "text-red-500",
+    slate: "text-slate-500",
+  };
+
+  return (
+    <>
+      <button
+        onClick={onClick}
+        className={`flex justify-center items-center text-xl w-8 h-8 rounded-full hover:bg-slate-200 ease-in-out duration-300 ${colorVariants[color]}`}
+      >
+        {Icon}
+      </button>
+    </>
+  );
+};
+
+export default ActionButton;
