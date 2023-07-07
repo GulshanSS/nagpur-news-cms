3a1992cefe412b8c69c943ea8fc56e4cbb8bf05b
GulshanSS <sahugulshan686@gmail.com>
2023-07-07 19:34:25 +0530

    Add: category API

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -3,16 +3,22 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { authApi } from "./api/authApi";
 import { userApi } from "./api/userApi";
 import userReducer from "./features/userSlice";
+import { categoryApi } from "./api/categoryApi";
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
+    [categoryApi.reducerPath]: categoryApi.reducer,
     userState: userReducer,
   },
   devTools: true,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({}).concat([authApi.middleware, userApi.middleware]),
+    getDefaultMiddleware({}).concat([
+      authApi.middleware,
+      userApi.middleware,
+      categoryApi.middleware,
+    ]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
