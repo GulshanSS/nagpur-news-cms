75fa423dcd7ef3f7093b40fc4211be7d51ee99e6
GulshanSS <sahugulshan686@gmail.com>
2023-07-07 19:30:15 +0530

    Add: View Category Component

---
+++ client/src/components/Category/ViewCategory.tsx
@@ -0,0 +1,115 @@
+import { MdDelete } from "react-icons/md";
+import {
+  useDeleteCategoryMutation,
+  useGetCategoryQuery,
+} from "../../redux/api/categoryApi";
+import ActionButton from "../ActionButton";
+import Status from "../Status";
+import { HiPencil } from "react-icons/hi";
+import Modal from "../Modal";
+import CategoryForm from "../Forms/CategoryForm";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { APIErrorResponse } from "../../redux/api/types";
+
+type Props = {
+  id: string;
+};
+
+const ViewCategory = ({ id }: Props) => {
+  const [modalCloseForm, setModalCloseForm] = useState<boolean>(false);
+
+  const { data, isLoading, isError, error } = useGetCategoryQuery(id);
+
+  const [deleteCategory] = useDeleteCategoryMutation();
+
+  if (isError) {
+    toast.error((error as APIErrorResponse).data.message);
+  }
+
+  if (isLoading) {
+    return <>loading...</>;
+  }
+
+  return (
+    <>
+      <div className="w-80 md:w-96 flex gap-4 flex-col items-center bg-slate-100 rounded-md p-5 shadow-md">
+        <div className="w-full flex flex-col">
+          <span className="w-28 text-[12px] font-bold text-slate-800">ID</span>
+          <span className="font-bold text-lg text-slate-500">
+            {data?.category.id}
+          </span>
+        </div>
+        <div className="w-full flex flex-col">
+          <span className="w-28 text-[12px] font-bold text-slate-800">
+            Name
+          </span>
+          <span className="font-bold text-lg text-slate-500">
+            {data?.category.name}
+          </span>
+        </div>
+        <div className="w-full flex flex-col">
+          <span className="w-28 text-[12px] font-bold text-slate-800">
+            Created At
+          </span>
+          <span className="font-bold text-lg text-slate-500">
+            {new Intl.DateTimeFormat("en-GB", {
+              year: "numeric",
+              month: "long",
+              day: "2-digit",
+            }).format(new Date(data?.category.createdAt!))}
+          </span>
+        </div>
+        <div className="w-full flex flex-col">
+          <span className="w-28 text-[12px] font-bold text-slate-800">
+            Updated At
+          </span>
+          <span className="font-bold text-lg text-slate-500">
+            {new Intl.DateTimeFormat("en-GB", {
+              year: "numeric",
+              month: "long",
+              day: "2-digit",
+            }).format(new Date(data?.category.updatedAt!))}
+          </span>
+        </div>
+        <div className="w-full flex justify-between items-center gap-1">
+          <div>
+            {data?.category.active ? (
+              <Status label="Active" colorVariant="green" />
+            ) : (
+              <Status label="Active" colorVariant="slate" />
+            )}
+          </div>
+          <div className="flex">
+            <ActionButton
+              onClick={() => setModalCloseForm(true)}
+              Icon={<HiPencil />}
+              color="blue"
+            />
+            <ActionButton
+              onClick={() => deleteCategory(id)}
+              Icon={<MdDelete />}
+              color="red"
+            />
+          </div>
+        </div>
+      </div>
+      <Modal
+        id="categoryUpdateForm"
+        close={modalCloseForm}
+        setClose={setModalCloseForm}
+      >
+        <CategoryForm
+          buttonLabel="Update"
+          category={{
+            id,
+            name: data?.category.name,
+            active: data?.category.active,
+          }}
+        />
+      </Modal>
+    </>
+  );
+};
+
+export default ViewCategory;
