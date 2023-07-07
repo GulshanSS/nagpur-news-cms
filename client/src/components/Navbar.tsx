004ea20d1802634b92773b70622d582e95ea4b6b
GulshanSS <sahugulshan686@gmail.com>
2023-07-07 19:28:57 +0530

    Add: Navbar Component

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,100 @@
+import { IoLogOut } from "react-icons/io5";
+
+type Props = {
+  children: JSX.Element;
+};
+
+const Navbar = ({ children }: Props) => {
+  return (
+    <>
+      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start">
+              <button
+                data-drawer-target="logo-sidebar"
+                data-drawer-toggle="logo-sidebar"
+                aria-controls="logo-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                  ></path>
+                </svg>
+              </button>
+              <a href="#" className="flex ml-2 md:mr-24">
+                <img
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  className="h-8 mr-3"
+                  alt="FlowBite Logo"
+                />
+                <span className="self-center font-semibold text-2xl whitespace-nowrap">
+                  Nagpur News
+                </span>
+              </a>
+            </div>
+            <div className="flex items-center">
+              <div className="flex items-center ml-3">
+                <div>
+                  <button
+                    type="button"
+                    className="flex text-3xl bg-gray-200 rounded-full focus:outline-none"
+                    aria-expanded="false"
+                    data-dropdown-toggle="dropdown-user"
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <IoLogOut />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-[200px] h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                </svg>
+                <span className="ml-3">Category</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+      <div className="sm:ml-[200px]">{children}</div>
+    </>
+  );
+};
+
+export default Navbar;
