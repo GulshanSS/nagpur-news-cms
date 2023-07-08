aec5e29de88506d74a449d4e52610fc15f8de9aa
GulshanSS <sahugulshan686@gmail.com>
2023-07-08 11:20:10 +0530

    Add: logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { IoLogOut } from "react-icons/io5";
+import Logo from "../assets/logo.jpg";
 
 type Props = {
   children: JSX.Element;
@@ -35,9 +36,9 @@ const Navbar = ({ children }: Props) => {
               </button>
               <a href="#" className="flex ml-2 md:mr-24">
                 <img
-                  src="https://flowbite.com/docs/images/logo.svg"
-                  className="h-8 mr-3"
-                  alt="FlowBite Logo"
+                  src={Logo}
+                  className="h-8 mr-3 rounded-full"
+                  alt="NagpurNews"
                 />
                 <span className="self-center font-semibold text-2xl whitespace-nowrap">
                   Nagpur News
@@ -70,6 +71,24 @@ const Navbar = ({ children }: Props) => {
       >
         <div className="h-full px-3 pb-4 overflow-y-auto bg-white">
           <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                </svg>
+                <span className="ml-3">Dashboard</span>
+              </a>
+            </li>
             <li>
               <a
                 href="#"
