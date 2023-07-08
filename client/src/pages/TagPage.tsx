36f5168b28b247c44cd5677d699b397e05a759eb
GulshanSS <sahugulshan686@gmail.com>
2023-07-08 11:24:23 +0530

    Add: Tag page Component

--- client/src/pages/TagPage.tsx
+++ client/src/pages/TagPage.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import TagForm from "../components/Forms/TagForm";
+import PageNav from "../components/PageNav";
+import AddResourceButton from "../components/AddResourceButton";
+import Modal from "../components/Modal";
+import DisplayAllTag from "../components/Tag/DisplayAllTag";
 
 const TagPage = () => {
+  const [modalCloseForm, setModalCloseForm] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   return (
     <>
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <h3 className="font-bold text-2xl mb-6">Tag</h3>
-        <TagForm />
+      <PageNav
+        setModalCloseForm={setModalCloseForm}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <div className="w-auto py-32">
+        <DisplayAllTag searchQuery={searchQuery} />
       </div>
+      <Modal id="tagForm" close={modalCloseForm} setClose={setModalCloseForm}>
+        <TagForm buttonLabel="Create" />
+      </Modal>
+      <AddResourceButton setModalCloseForm={setModalCloseForm} />
     </>
   );
 };
