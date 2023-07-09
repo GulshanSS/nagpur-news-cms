03ddde589721f9c10aa93bf736c08d507def2473
GulshanSS <sahugulshan686@gmail.com>
2023-07-10 00:15:03 +0530

    Add: Required controller and services

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -4,11 +4,24 @@ import {
   createUser,
   deleteUserById,
   getUserById,
+  getUserByRole,
+  verifyUser,
+  updateUserNameOrEmailById,
+  getUsersByName,
+  getUserByEmail,
+  resetPassword,
 } from "../service/user.service";
 import { AppError, HttpCode } from "../exceptions/AppError";
-import { CreateUserInput } from "../schemas/user.schema";
+import {
+  CreateUserInput,
+  ResetPasswordInput,
+  UpdateUserInput,
+} from "../schemas/user.schema";
 import { hash } from "../utils/bcrypt";
 import { Role } from "@prisma/client";
+import { sendEmail } from "../utils/email";
+import config from "../config";
+import { generatePassword } from "../utils/password";
 
 export const getLoggedInUserHandler = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -26,8 +39,13 @@ export const getLoggedInUserHandler = asyncHandler(
       success: true,
       user: {
         id: foundUser.id,
+        name: foundUser.name,
         email: foundUser.email,
+        role: foundUser.role,
+        active: foundUser.active,
         verified: foundUser.verified,
+        createdAt: foundUser.createdAt,
+        updatedAt: foundUser.updatedAt,
       },
     });
   }
@@ -59,6 +77,50 @@ export const getUserByIdHandler = asyncHandler(
   }
 );
 
+export const getAllUsersExceptAdminHandler = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await getUserByRole("TEAM");
+    if (users.length === 0) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: "No users found with Role TEAM",
+        })
+      );
+    }
+    return res.status(HttpCode.OK).json({
+      success: true,
+      users,
+      count: users.length,
+    });
+  }
+);
+
+export const getUsersExceptAdminByNameHandler = asyncHandler(
+  async (
+    req: Request<{ name: string }, {}, {}>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const name = req.params.name;
+    const users = await getUsersByName(name);
+    if (users.length === 0) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: `User not found with name ${name}`,
+        })
+      );
+    }
+
+    return res.status(HttpCode.OK).json({
+      success: true,
+      users,
+      count: users.length,
+    });
+  }
+);
+
 export const createUserHandler = asyncHandler(
   async (
     req: Request<{}, {}, CreateUserInput["body"]>,
@@ -66,16 +128,80 @@ export const createUserHandler = asyncHandler(
     next: NextFunction
   ) => {
     const data = req.body;
+    const existingUser = await getUserByEmail(data.email);
+    if (existingUser) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.BAD_REQUEST,
+          description: `User with ${existingUser.email} is already in use`,
+        })
+      );
+    }
+    const password = generatePassword();
     const createUserInput = {
       ...data,
-      password: await hash(data.password),
+      password: await hash(password),
       role: "TEAM" as Role,
     };
-    await createUser(createUserInput);
-
+    const user = await createUser(createUserInput);
+    await sendEmail(user.email, password, config.EMAIL_TYPE_ACCOUNT_CREATION);
     return res.status(HttpCode.CREATED).json({
       success: true,
-      message: "User created Successful",
+      message: `User created successfully with ${user.email}`,
+    });
+  }
+);
+
+export const resetPasswordHandler = asyncHandler(
+  async (
+    req: Request<{}, {}, ResetPasswordInput["body"]>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const userId: string = res.locals.payload.userId;
+    const existingUser = await getUserById(userId);
+    console.log(existingUser);
+    if (!existingUser) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: "User Not Found",
+        })
+      );
+    }
+    const hashedPassword = await hash(req.body.password);
+    await resetPassword(userId, hashedPassword);
+    return res.status(HttpCode.OK).json({
+      success: true,
+      message: `Password Reset Successfully`,
+    });
+  }
+);
+
+export const updateUserByIdHandler = asyncHandler(
+  async (
+    req: Request<UpdateUserInput["params"], {}, UpdateUserInput["body"]>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const userId = req.params.userId;
+    const existingUser = await getUserById(userId);
+    if (!existingUser) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: "User not found",
+        })
+      );
+    }
+    const data = req.body;
+    if (data?.email) {
+      await verifyUser(userId, false);
+    }
+    await updateUserNameOrEmailById(userId, data);
+    return res.status(HttpCode.OK).json({
+      success: true,
+      message: "User Successfully Updated",
     });
   }
 );
--- server/src/service/user.service.ts
+++ server/src/service/user.service.ts
@@ -1,19 +1,47 @@
 import { Role, User } from "@prisma/client";
-import { CreateUserInput } from "../schemas/user.schema";
+import { CreateUserInput, UpdateUserInput } from "../schemas/user.schema";
 import db from "../utils/db.server";
 
-export const createUser = async (user: CreateUserInput["body"]) => {
+export const createUser = async (
+  user: Pick<User, "email" | "name" | "password"> &
+    Partial<Pick<User, "active">>
+) => {
   return db.user.create({
     data: user,
   });
 };
 
-export const updateUserById = async (userId: string, user: Partial<User>) => {
+export const verifyUser = async (userId: string, verified: boolean) => {
   return await db.user.update({
     where: {
       id: parseInt(userId),
     },
-    data: user,
+    data: {
+      verified,
+    },
+  });
+};
+
+export const resetPassword = async (id: string, password: string) => {
+  return await db.user.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      password,
+    },
+  });
+};
+
+export const updateUserNameOrEmailById = async (
+  userId: string,
+  user: Partial<UpdateUserInput["body"]>
+) => {
+  return await db.user.update({
+    where: {
+      id: parseInt(userId),
+    },
+    data: user!,
   });
 };
 
@@ -45,6 +73,17 @@ export const getAllUsers = async () => {
   return await db.user.findMany({
     where: {
       role: "TEAM",
+    },
+  });
+};
+
+export const getUsersByName = async (name: string) => {
+  return await db.user.findMany({
+    where: {
+      role: "TEAM",
+      name: {
+        contains: name,
+      },
     },
   });
 };
