bb5e6bf8131fd416443bcd228c1db9cad318947a
GulshanSS <sahugulshan686@gmail.com>
2023-07-09 23:50:40 +0530

    Remove: Hover Effect

--- client/src/components/ActionButton.tsx
+++ client/src/components/ActionButton.tsx
@@ -17,7 +17,7 @@ const ActionButton = ({ onClick, Icon, color }: Props) => {
     <>
       <button
         onClick={onClick}
-        className={`flex justify-center items-center text-xl w-8 h-8 rounded-full hover:bg-slate-200 ease-in-out duration-300 ${colorVariants[color]}`}
+        className={`flex justify-center items-center text-xl w-8 h-8 rounded-full bg-slate-200 ease-in-out duration-300 ${colorVariants[color]}`}
       >
         {Icon}
       </button>
