aa1658b14412c32e528f8a0b38f18481d6f5d4e2
GulshanSS <sahugulshan686@gmail.com>
2023-07-10 00:17:24 +0530

    Fix: Routing && Add: Restriction middleware

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,10 @@ import TagPage from "./pages/TagPage";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import Layout from "./layout/Layout";
+import UserPage from "./pages/UserPage";
+import MePage from "./pages/MePage";
+import UnAuthorizedPage from "./pages/UnAuthorizedPage";
+import RequireUser from "./components/Auth/RequireUser";
 
 function App() {
   return (
@@ -15,10 +19,19 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/verify-otp" element={<OTPPage />} />
-          <Route element={<Layout />}>
-            <Route path="/category" element={<CategoryPage />} />
-            <Route path="/tag" element={<TagPage />} />
+          <Route element={<RequireUser allowedRoles={["ADMIN", "TEAM"]} />}>
+            <Route element={<Layout />}>
+              <Route path="/me" element={<MePage />} />
+              <Route path="/category" element={<CategoryPage />} />
+              <Route path="/tag" element={<TagPage />} />
+            </Route>
           </Route>
+          <Route element={<RequireUser allowedRoles={["ADMIN"]} />}>
+            <Route element={<Layout />}>
+              <Route path="/user" element={<UserPage />} />
+            </Route>
+          </Route>
+          <Route path="/unauthorized" element={<UnAuthorizedPage />} />
         </Routes>
       </BrowserRouter>
       <ToastContainer
