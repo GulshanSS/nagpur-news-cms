17c90a07f97d1b73522bb08172f8078574cd866d
GulshanSS <sahugulshan686@gmail.com>
2023-07-10 00:14:39 +0530

    Fix: Added  & changed required schema

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -30,16 +30,32 @@ const params = {
   }),
 };
 
-export const createUserSchema = object({ ...payload });
-export const loginUserSchema = object({
-  body: payload.body.omit({ name: true, role: true, active: true }),
+export const createUserSchema = object({
+  body: payload.body.omit({ password: true, role: true }),
 });
-export const updateUserSchema = object({ ...params, ...payload });
+export const loginUserSchema = object({
+  body: object({
+    email: string()
+      .min(1, "Email is required")
+      .email("Email address is invalid"),
+    password: string().min(8, "Must be at least 8 characters in length"),
+  }),
+});
+export const updateUserSchema = object({
+  ...params,
+  body: payload.body
+    .omit({ role: true, active: true, password: true })
+    .optional(),
+});
+export const resetPasswordSchema = object({
+  body: payload.body.pick({ password: true }),
+});
 export const getUserSchema = object({ ...params });
 export const deleteUserSchema = object({ ...params });
 
 export type CreateUserInput = TypeOf<typeof createUserSchema>;
 export type LoginUserInput = TypeOf<typeof loginUserSchema>;
 export type UpdateUserInput = TypeOf<typeof updateUserSchema>;
+export type ResetPasswordInput = TypeOf<typeof resetPasswordSchema>;
 export type GetUserInput = TypeOf<typeof getUserSchema>;
 export type DeleteUserInput = TypeOf<typeof deleteUserSchema>;
