321880762d67a05aad8004c374f3805d8bf85a43
GulshanSS <sahugulshan686@gmail.com>
2023-07-09 23:58:55 +0530

    Fix: Login Validation Schema

--- client/src/validationSchema/LoginSchema.ts
+++ client/src/validationSchema/LoginSchema.ts
@@ -8,13 +8,6 @@ export const LoginSchema = z.object({
     .email("Invalid Email. Try again with valid email"),
   password: z
     .string()
-    .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
-    .regex(new RegExp(".*[a-z].*"), "One lowercase character")
-    .regex(new RegExp(".*\\d.*"), "One number")
-    .regex(
-      new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
-      "One special character"
-    )
     .min(8, "Must be at least 8 characters in length"),
 });
 
