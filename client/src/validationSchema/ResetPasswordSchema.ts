b7f1a3ae5526dcf1754349938132d90e963c5d39
GulshanSS <sahugulshan686@gmail.com>
2023-07-09 23:59:13 +0530

    Add: Reset password Validation Schema

---
+++ client/src/validationSchema/ResetPasswordSchema.ts
@@ -0,0 +1,29 @@
+import { TypeOf, z, string } from "zod";
+
+export const ResetPasswordSchema = z
+  .object({
+    password: string()
+      .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
+      .regex(new RegExp(".*[a-z].*"), "One lowercase character")
+      .regex(new RegExp(".*\\d.*"), "One number")
+      .regex(
+        new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
+        "One special character"
+      )
+      .min(8, "Must be at least 8 characters in length"),
+    confirmPassword: string()
+      .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
+      .regex(new RegExp(".*[a-z].*"), "One lowercase character")
+      .regex(new RegExp(".*\\d.*"), "One number")
+      .regex(
+        new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
+        "One special character"
+      )
+      .min(8, "Must be at least 8 characters in length"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
+
+export type ResetPasswordInput = TypeOf<typeof ResetPasswordSchema>;
