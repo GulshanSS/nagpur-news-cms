f6f7a29e6d3dd65860bee12127d1a220916cdc69
GulshanSS <sahugulshan686@gmail.com>
2023-07-09 23:59:28 +0530

    Add: User Validation Schema

---
+++ client/src/validationSchema/UserSchema.ts
@@ -0,0 +1,9 @@
+import { TypeOf, z } from "zod";
+
+export const UserSchema = z.object({
+  name: z.string().min(1, "User name is required"),
+  email: z.string().email("Invalid email. Enter valid email"),
+  active: z.boolean().optional(),
+});
+
+export type UserInput = TypeOf<typeof UserSchema>;
