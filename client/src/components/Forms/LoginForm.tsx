c7de5134e8b54bce40fd833e3e516b0ba2292df7
GulshanSS <sahugulshan686@gmail.com>
2023-07-11 12:29:15 +0530

    Add: Forgot Password UI

--- client/src/components/Forms/LoginForm.tsx
+++ client/src/components/Forms/LoginForm.tsx
@@ -5,9 +5,10 @@ import { LoginSchema, LoginInput } from "../../validationSchema/LoginSchema";
 import { useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { useLoginUserMutation } from "../../redux/api/authApi";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { OTPErrorResponse } from "../../redux/api/types";
 import { toast } from "react-toastify";
+import { BsQuestionCircleFill } from "react-icons/bs";
 
 const LoginForm = () => {
   const methods = useForm<LoginInput>({ resolver: zodResolver(LoginSchema) });
@@ -73,6 +74,16 @@ const LoginForm = () => {
             type="password"
             placeholder="Enter Password"
           />
+          <div className="w-full flex justify-end mb-6 text-blue-500 font-bold">
+            <Link to="/forgot-password">
+              <div className="w-fit flex">
+                <span className="text-[12px]">Forgot Password</span>
+                <span className="ml-2">
+                  <BsQuestionCircleFill />
+                </span>
+              </div>
+            </Link>
+          </div>
           <SubmitButton label="Login" />
         </form>
       </FormProvider>
