ed05634cd24ead0b0deede60d0c149d273ce5aa0
GulshanSS <sahugulshan686@gmail.com>
2023-07-11 12:34:37 +0530

    Add: Routes related to forgot password

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,14 +3,27 @@ import {
   loginUserHandler,
   logoutHandler,
   refreshTokenHandler,
+  resetPasswordByTokenHandler,
+  sendRestPasswordLinkHandler,
 } from "../controller/auth.controller";
 import validateSchema from "../middleware/validateSchema";
-import { loginUserSchema } from "../schemas/user.schema";
+import {
+  loginUserSchema,
+  sendRestPasswordLinkSchema,
+} from "../schemas/user.schema";
 import { refreshTokenSchema } from "../schemas/refreshToken.schema";
+import { isAuthenticated } from "../middleware/isAuthenticated";
+import { authorize } from "../middleware/authorize";
 
 const AuthRouter = Router();
 
 AuthRouter.post("/login", validateSchema(loginUserSchema), loginUserHandler);
+AuthRouter.post(
+  "/reset-password-link",
+  validateSchema(sendRestPasswordLinkSchema),
+  sendRestPasswordLinkHandler
+);
+AuthRouter.post("/reset-password/:token", resetPasswordByTokenHandler);
 AuthRouter.get(
   "/refresh-token",
   validateSchema(refreshTokenSchema),
