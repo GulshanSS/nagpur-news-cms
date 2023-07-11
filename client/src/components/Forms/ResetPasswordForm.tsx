4b5db9a22b87240951d9415c4dcab9dbd0f4041a
GulshanSS <sahugulshan686@gmail.com>
2023-07-11 12:29:43 +0530

    Fix: Reusable reset password form

--- client/src/components/Forms/ResetPasswordForm.tsx
+++ client/src/components/Forms/ResetPasswordForm.tsx
@@ -11,8 +11,18 @@ import { useResetPasswordMutation } from "../../redux/api/userApi";
 import { toast } from "react-toastify";
 import { APIErrorResponse } from "../../redux/api/types";
 import { TiTick } from "react-icons/ti";
+import { useResetPasswordByTokenMutation } from "../../redux/api/authApi";
+import LinkButton from "../LinkButton";
+import { useSearchParams } from "react-router-dom";
 
-const ResetPasswordForm = () => {
+type Props = {
+  bg: boolean;
+  token?: string;
+};
+
+const ResetPasswordForm = ({ bg, token }: Props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const methods = useForm<ResetPasswordInput>({
     resolver: zodResolver(ResetPasswordSchema),
   });
@@ -23,18 +33,48 @@ const ResetPasswordForm = () => {
     formState: { isSubmitSuccessful },
   } = methods;
 
-  const [resetPassword, { data, isLoading, isSuccess, error, isError }] =
-    useResetPasswordMutation();
+  const [
+    resetPassword,
+    {
+      data: resetPasswordResult,
+      isLoading: isResetPasswordLoading,
+      isSuccess: isResetPasswordSuccess,
+      error: resetPasswordError,
+      isError: isResetPasswordError,
+    },
+  ] = useResetPasswordMutation();
+
+  const [
+    resetPasswordByToken,
+    {
+      data: resetPasswordByTokenResult,
+      isLoading: isResetPasswordByTokenLoading,
+      isSuccess: isResetPasswordByTokenSuccess,
+      error: resetPasswordByTokenError,
+      isError: isResetPasswordByTokenError,
+    },
+  ] = useResetPasswordByTokenMutation();
 
   useEffect(() => {
-    if (isSuccess) {
-      toast.success("Password reset successfully");
+    if (isResetPasswordSuccess || isResetPasswordByTokenSuccess) {
+      if (token) {
+        setSearchParams((params: any) => {
+          params.delete("token");
+          return params;
+        });
+      }
+      toast.success(
+        (resetPasswordResult || resetPasswordByTokenResult)?.message
+      );
     }
 
-    if (isError) {
-      toast.error((error as APIErrorResponse).data.message);
+    if (isResetPasswordError || isResetPasswordByTokenError) {
+      toast.error(
+        ((resetPasswordError || resetPasswordByTokenError) as APIErrorResponse)
+          .data.message
+      );
     }
-  }, [isLoading]);
+  }, [isResetPasswordLoading, isResetPasswordByTokenLoading]);
 
   useEffect(() => {
     if (isSubmitSuccessful) {
@@ -43,21 +83,35 @@ const ResetPasswordForm = () => {
   });
 
   const handleResetPasswordSubmit = (values: ResetPasswordInput) => {
-    resetPassword(values);
+    if (token) {
+      resetPasswordByToken({ token, ...values });
+    } else {
+      resetPassword(values);
+    }
   };
 
   return (
     <>
-      <div className="w-80 md:w-96 mx-2 bg-slate-200 px-3 py-5 rounded-md">
-        {data?.success ? (
-          <div className="flex flex-col justify-center items-center">
+      <div
+        className={`w-80 md:w-96 mx-2 ${
+          bg && "bg-slate-100"
+        } px-3 py-5 rounded-md`}
+      >
+        {(resetPasswordByTokenResult || resetPasswordResult)?.success ? (
+          <div className="flex flex-col justify-center items-center my-6">
             <div className="w-20 h-20 flex justify-center items-center bg-green-500 text-white rounded-full text-6xl mb-6">
               <TiTick />
             </div>
             <span>Password Reset Successfully</span>
+            {resetPasswordByTokenResult && (
+              <LinkButton to="/login" label="Back to Login" />
+            )}
           </div>
         ) : (
           <FormProvider {...methods}>
+            {token && (
+              <h3 className="font-bold text-2xl mb-6">Reset Password</h3>
+            )}
             <form
               onSubmit={handleSubmit(handleResetPasswordSubmit)}
               noValidate
