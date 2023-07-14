897b6e0f3d67a47fabc1281672e42ef2e79146f2
GulshanSS <sahugulshan686@gmail.com>
2023-07-14 12:11:46 +0530

    Add: Testimonial Page component

---
+++ client/src/pages/TestimonialPage.tsx
@@ -0,0 +1,13 @@
+import TestimonialForm from "../components/Forms/TestimonialForm";
+
+const TestimonialPage = () => {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <TestimonialForm />
+      </div>
+    </>
+  );
+};
+
+export default TestimonialPage;
