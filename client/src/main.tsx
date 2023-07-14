80f79be47d5b531ed79524b19d50f486ad2d11be
GulshanSS <sahugulshan686@gmail.com>
2023-07-14 12:09:50 +0530

    Add: AuthMiddleware component and wrap whole app inside it

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.tsx";
+import { CookiesProvider } from "react-cookie";
+import AuthMiddleware from "./components/AuthMiddleware.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <CookiesProvider>
+      <AuthMiddleware>
+        <App />
+      </AuthMiddleware>
+    </CookiesProvider>
   </Provider>
 );
