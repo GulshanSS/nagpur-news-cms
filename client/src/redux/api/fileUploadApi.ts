508dd8a06b9e824daf6b4ec8f747c7a4712af24e
GulshanSS <sahugulshan686@gmail.com>
2023-07-17 22:37:55 +0530

    Add: FileUploadApi

---
+++ client/src/redux/api/fileUploadApi.ts
@@ -0,0 +1,25 @@
+import { createApi } from "@reduxjs/toolkit/dist/query/react";
+import baseQueryWithReAuth from "../baseQueryWithReAuth";
+import { Media } from "./types";
+
+export const fileUploadApi = createApi({
+  reducerPath: "fileUploadApi",
+  baseQuery: baseQueryWithReAuth,
+  tagTypes: ["Image"],
+  endpoints: (builder) => ({
+    uploadSingleFile: builder.mutation<
+      { success: boolean; media: Media },
+      FormData
+    >({
+      query: (data) => ({
+        url: "/media/upload/single",
+        method: "POST",
+        body: data,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Image"],
+    }),
+  }),
+});
+
+export const { useUploadSingleFileMutation } = fileUploadApi;
