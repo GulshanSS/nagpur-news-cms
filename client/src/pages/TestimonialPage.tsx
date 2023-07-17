667196e2b66706cedfc1983903c283a82876d945
GulshanSS <sahugulshan686@gmail.com>
2023-07-17 22:37:13 +0530

    Fix: Testimonial Page component

--- client/src/pages/TestimonialPage.tsx
+++ client/src/pages/TestimonialPage.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import TestimonialForm from "../components/Forms/TestimonialForm";
+import Modal from "../components/Modal";
+import PageNav from "../components/PageNav";
+import AddResourceButton from "../components/AddResourceButton";
+import DisplayAllTestimonial from "../components/Testimonial/DisplayAllTestimonial";
 
 const TestimonialPage = () => {
+  const [modalCloseForm, setModalCloseForm] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   return (
     <>
-      <div className="w-screen h-screen flex justify-center items-center">
-        <TestimonialForm />
+      <PageNav
+        setModalCloseForm={setModalCloseForm}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <div className="w-auto py-32">
+        <DisplayAllTestimonial searchQuery={searchQuery} />
       </div>
+      <Modal
+        id="categoryForm"
+        close={modalCloseForm}
+        setClose={setModalCloseForm}
+      >
+        <TestimonialForm buttonLabel="Create" />
+      </Modal>
+      <AddResourceButton setModalCloseForm={setModalCloseForm} />
     </>
   );
 };
