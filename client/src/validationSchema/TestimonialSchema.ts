00a71cc50e78854e3719a3db74f6c556e98dda2f
GulshanSS <sahugulshan686@gmail.com>
2023-07-17 22:38:46 +0530

    Fix: Testimonial Schema

--- client/src/validationSchema/TestimonialSchema.ts
+++ client/src/validationSchema/TestimonialSchema.ts
@@ -1,10 +1,55 @@
 import { TypeOf, z } from "zod";
 
-export const TestimonialSchema = z.object({
-  media: z.instanceof(FileList),
-  quote: z.string().optional(),
-  quotedBy: z.string().optional(),
-  active: z.boolean().optional(),
-});
+const MAX_FILE_SIZE = 2000000;
+
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpg",
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+];
+
+const ACCEPTED_VIDEO_TYPES = ["video/mp4", "video/webm", "video/ogg"];
+
+export const TestimonialSchema = z
+  .object({
+    media: z
+      .array(z.custom<File>())
+      .refine(
+        (files) => {
+          return files.every((file) => file instanceof File);
+        },
+        {
+          message: "Expected a file",
+        }
+      )
+      .refine(
+        (files) => files.every((file) => file.size <= MAX_FILE_SIZE),
+        `File size should be less than 2mb.`
+      )
+      .refine(
+        (files) =>
+          files.every(
+            (file) =>
+              ACCEPTED_IMAGE_TYPES.includes(file.type) ||
+              ACCEPTED_VIDEO_TYPES.includes(file.type)
+          ),
+        "Only these types are allowed .jpg, .jpeg, .png, .webp, .mp4, .webm and .ogg"
+      ),
+    quote: z.string().trim().optional(),
+    quotedBy: z.string().trim().min(1, "Quoted By is required"),
+    designation: z.string().trim().optional(),
+    active: z.boolean().optional(),
+  })
+  .refine(
+    (schema) =>
+      schema.media.every((file) => ACCEPTED_IMAGE_TYPES.includes(file.type)) &&
+      schema.quote !== "" &&
+      schema.quote !== undefined,
+    {
+      path: ["quote"],
+      message: "Quote is required",
+    }
+  );
 
 export type TestimonialInput = TypeOf<typeof TestimonialSchema>;
