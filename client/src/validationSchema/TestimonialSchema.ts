4a963b593b8ee3a92ed82a0bfe0ad73506eb3e2a
GulshanSS <sahugulshan686@gmail.com>
2023-07-18 19:36:42 +0530

    Add: CreateTestimonial & UpdateTestimonial validation schema

--- client/src/validationSchema/TestimonialSchema.ts
+++ client/src/validationSchema/TestimonialSchema.ts
@@ -11,7 +11,49 @@ const ACCEPTED_IMAGE_TYPES = [
 
 const ACCEPTED_VIDEO_TYPES = ["video/mp4", "video/webm", "video/ogg"];
 
-export const TestimonialSchema = z
+export const CreateTestimonialSchema = z
+  .object({
+    media: z
+      .array(z.custom<File>())
+      .length(1, "File is required")
+      .refine(
+        (files) => {
+          return files.every((file) => file instanceof File);
+        },
+        {
+          message: "Expected a file",
+        }
+      )
+      .refine(
+        (files) => files.every((file) => file.size <= MAX_FILE_SIZE),
+        `File size should be less than 2mb.`
+      )
+      .refine(
+        (files) =>
+          files.every(
+            (file) =>
+              ACCEPTED_IMAGE_TYPES.includes(file.type) ||
+              ACCEPTED_VIDEO_TYPES.includes(file.type)
+          ),
+        "Only these types are allowed .jpg, .jpeg, .png, .webp, .mp4, .webm and .ogg"
+      ),
+    quote: z.string().trim().optional(),
+    quotedBy: z.string().trim().min(1, "Quoted By is required"),
+    designation: z.string().trim().optional(),
+    active: z.boolean().optional(),
+  })
+  .refine(
+    (schema) =>
+      schema.media.every((file) => ACCEPTED_IMAGE_TYPES.includes(file.type)) &&
+      schema.quote !== "" &&
+      schema.quote !== undefined,
+    {
+      path: ["quote"],
+      message: "Quote is required",
+    }
+  );
+
+export const UpdateTestimonialSchema = z
   .object({
     media: z
       .array(z.custom<File>())
@@ -52,4 +94,5 @@ export const TestimonialSchema = z
     }
   );
 
-export type TestimonialInput = TypeOf<typeof TestimonialSchema>;
+export type CreateTestimonialInput = TypeOf<typeof CreateTestimonialSchema>;
+export type UpdateTestimonialInput = TypeOf<typeof UpdateTestimonialSchema>;
