117eed0d5f0ce5a2e665ab62920d15d933252fc3
GulshanSS <sahugulshan686@gmail.com>
2023-07-18 19:39:06 +0530

    Add: Update Media controller

--- server/src/controller/media.Controller.ts
+++ server/src/controller/media.Controller.ts
@@ -3,7 +3,12 @@ import asyncHandler from "../middleware/asyncHandler";
 import { getSignedUrlForMedia, uploadSingleFile } from "../utils/s3";
 import { AppError, HttpCode } from "../exceptions/AppError";
 import { randomImageName } from "../utils/imageName";
-import { createMedia, getAllMedia } from "../service/media.service";
+import {
+  createMedia,
+  getAllMedia,
+  getMediaById,
+  updateMedia,
+} from "../service/media.service";
 
 export const getAllMediaHandler = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -50,7 +55,58 @@ export const uploadSingleFileHandler = asyncHandler(
         })
       );
     }
-    const media = await createMedia({ type: file.mimetype, key: imageName });
+    const testimonialId = req.body.testimonialId as string;
+    const media = await createMedia({
+      type: file.mimetype,
+      key: imageName,
+      testimonialId: parseInt(testimonialId),
+    });
+    return res.status(HttpCode.CREATED).json({
+      success: true,
+      media,
+    });
+  }
+);
+
+export const updateMediaByIdHandler = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const mediaId = req.body.mediaId as string;
+
+    const existingMedia = await getMediaById(parseInt(mediaId));
+
+    if (!existingMedia) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.NOT_FOUND,
+          description: "Media not found",
+        })
+      );
+    }
+
+    const file = req.file as Express.Multer.File;
+    if (!file) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.BAD_REQUEST,
+          description: "File not provided",
+        })
+      );
+    }
+    const imageName = randomImageName();
+    const result = await uploadSingleFile(file, imageName);
+    if (!result) {
+      return next(
+        new AppError({
+          httpCode: HttpCode.BAD_REQUEST,
+          description: "Error uploading File",
+        })
+      );
+    }
+
+    const media = await updateMedia(parseInt(mediaId), {
+      type: file.mimetype,
+      key: imageName,
+    });
     return res.status(HttpCode.CREATED).json({
       success: true,
       media,
