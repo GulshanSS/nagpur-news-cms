bc1b12a5febf9f4a1cb02ad64f3cfd0ff9fc1e16
GulshanSS <sahugulshan686@gmail.com>
2023-07-21 13:53:45 +0530

    Fix: UI changes

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,9 +3,11 @@ import LoginForm from "../components/Forms/LoginForm";
 function LoginPage() {
   return (
     <>
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <h3 className="font-bold text-2xl mb-6">Admin Panel</h3>
-        <LoginForm />
+      <div className="w-screen h-screen flex flex-col justify-center items-center bg-[#FEF9D5]">
+        <div className="bg-white bg-opacity-60 px-2.5 py-6 rounded-2xl shadow-md text-[#614E5C]">
+          <h3 className="font-bold text-3xl mb-6 text-center">Login</h3>
+          <LoginForm />
+        </div>
       </div>
     </>
   );
