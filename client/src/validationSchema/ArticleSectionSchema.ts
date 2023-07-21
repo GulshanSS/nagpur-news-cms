9b5ceeb2a3ed034f61b3f4f971a2c87752a5778e
GulshanSS <sahugulshan686@gmail.com>
2023-07-21 13:56:01 +0530

    Add: Article Section Schema

---
+++ client/src/validationSchema/ArticleSectionSchema.ts
@@ -0,0 +1,84 @@
+import { TypeOf, z } from "zod";
+
+const MAX_FILE_SIZE = 2000000;
+
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpg",
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+];
+
+const ACCEPTED_VIDEO_TYPES = ["video/mp4", "video/webm", "video/ogg"];
+
+export const CreateArticleSectionSchema = z.object({
+  media: z
+    .array(z.custom<File>())
+    .length(1, "File is required")
+    .refine(
+      (files) => {
+        return files.every((file) => file instanceof File);
+      },
+      {
+        message: "Expected a file",
+      }
+    )
+    .refine(
+      (files) => files.every((file) => file.size <= MAX_FILE_SIZE),
+      `File size should be less than 2mb.`
+    )
+    .refine(
+      (files) =>
+        files.every(
+          (file) =>
+            ACCEPTED_IMAGE_TYPES.includes(file.type) ||
+            ACCEPTED_VIDEO_TYPES.includes(file.type)
+        ),
+      "Only these types are allowed .jpg, .jpeg, .png, .webp, .mp4, .webm and .ogg"
+    ),
+  title: z.string().trim().optional(),
+  content: z.string().trim().min(1, "Content is required"),
+  sequence: z
+    .string()
+    .trim()
+    .regex(new RegExp("\\d{1}"), "Sequence should contain 0-9 number"),
+});
+
+export const UpdateArticleSectionSchema = z.object({
+  media: z
+    .array(z.custom<File>())
+    .refine(
+      (files) => {
+        return files.every((file) => file instanceof File);
+      },
+      {
+        message: "Expected a file",
+      }
+    )
+    .refine(
+      (files) => files.every((file) => file.size <= MAX_FILE_SIZE),
+      `File size should be less than 2mb.`
+    )
+    .refine(
+      (files) =>
+        files.every(
+          (file) =>
+            ACCEPTED_IMAGE_TYPES.includes(file.type) ||
+            ACCEPTED_VIDEO_TYPES.includes(file.type)
+        ),
+      "Only these types are allowed .jpg, .jpeg, .png, .webp, .mp4, .webm and .ogg"
+    ),
+  title: z.string().trim().optional(),
+  content: z.string().trim().min(1, "Content is required"),
+  sequence: z
+    .string()
+    .trim()
+    .regex(new RegExp("\\d{1}"), "Sequence should contain 0-9 number"),
+});
+
+export type CreateArticleSectionInput = TypeOf<
+  typeof CreateArticleSectionSchema
+>;
+export type UpdateArticleSectionInput = TypeOf<
+  typeof UpdateArticleSectionSchema
+>;
