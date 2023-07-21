0003bba86560399fd16b2e2a104a5ca33b9546b5
GulshanSS <sahugulshan686@gmail.com>
2023-07-21 13:51:44 +0530

    Fix: Ui changes

--- client/src/components/FormComponents/SubmitButton.tsx
+++ client/src/components/FormComponents/SubmitButton.tsx
@@ -7,7 +7,7 @@ const SubmitButton = ({ label }: Props) => {
     <>
       <button
         type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-full sm:w-auto text-sm px-5 py-2.5 text-center"
+        className="text-[#FEF9D5] bg-[#614E5C] hover:shadow-xl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-full sm:w-auto text-sm px-5 py-2.5 text-center"
       >
         {label}
       </button>
