9f895afde95606a73997b814b6aee52fbe57aceb
GulshanSS <sahugulshan686@gmail.com>
2023-07-22 19:44:01 +0530

    Add: Delete functionality to image card if id is passed to the component

--- client/src/components/ImageCard.tsx
+++ client/src/components/ImageCard.tsx
@@ -1,16 +1,32 @@
+import { MdDelete } from "react-icons/md";
+import { useDeletMediaMutation } from "../redux/api/fileUploadApi";
+import ActionButton from "./ActionButton";
+
 type Props = {
+  id?: number;
   url: string;
   name: string;
 };
 
-const ImageCard = ({ url, name }: Props) => {
+const ImageCard = ({ id, url, name }: Props) => {
+  const [deleteMedia] = useDeletMediaMutation();
+
   return (
     <>
-      <img
-        className="h-20 rounded-md border border-slate-200"
-        src={url}
-        alt={name}
-      />
+      <div>
+        <img
+          className="h-20 rounded-md border border-slate-200"
+          src={url}
+          alt={name}
+        />
+        {id && (
+          <ActionButton
+            onClick={() => deleteMedia(id)}
+            Icon={<MdDelete />}
+            color="red"
+          />
+        )}
+      </div>
     </>
   );
 };
