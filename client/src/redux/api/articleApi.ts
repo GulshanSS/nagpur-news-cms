9e599d18099ef5d7e590bacef39259313ba62dab
GulshanSS <sahugulshan686@gmail.com>
2023-07-22 19:47:36 +0530

    Add: Update and Get endpoints to article Api

--- client/src/redux/api/articleApi.ts
+++ client/src/redux/api/articleApi.ts
@@ -1,7 +1,10 @@
 import { createApi } from "@reduxjs/toolkit/dist/query/react";
 import baseQueryWithReAuth from "../baseQueryWithReAuth";
 import { Article } from "./types";
-import { CreateArticleInput } from "../../validationSchema/ArticleSchema";
+import {
+  CreateArticleInput,
+  UpdateArticleInput,
+} from "../../validationSchema/ArticleSchema";
 import { fileUploadApi } from "./fileUploadApi";
 
 export const articleApi = createApi({
@@ -45,7 +48,82 @@ export const articleApi = createApi({
         }
       },
     }),
+    updateArticle: builder.mutation<
+      {
+        success: boolean;
+        message: string;
+      },
+      UpdateArticleInput & { state: String; articleId: number }
+    >({
+      query: (data) => {
+        const { media, articleId, ...rest } = data;
+        return {
+          url: `/article/${articleId}/update`,
+          method: "PUT",
+          body: {
+            ...rest,
+            category: [...rest.category].map(({ value }) => value),
+            tag: [...rest.tag].map(({ value }) => value),
+          },
+          credentials: "include",
+        };
+      },
+      async onQueryStarted(args, { dispatch, queryFulfilled }) {
+        try {
+          if (args.media.length > 0) {
+            await queryFulfilled;
+            const formData = new FormData();
+            args.media.forEach((media) => formData.append("files", media));
+            formData.append("articleId", args.articleId.toString());
+            await dispatch(
+              fileUploadApi.endpoints.uploadMutipleFileForArticle.initiate(
+                formData
+              )
+            );
+          }
+          dispatch(articleApi.util.invalidateTags(["Article"]));
+        } catch (e: unknown) {
+          if (e instanceof Error) console.log(e);
+        }
+      },
+    }),
+    getAllArticle: builder.query<
+      { success: boolean; articles: Article[] },
+      void
+    >({
+      query: () => "/article",
+      providesTags: ["Article"],
+    }),
+    getArticle: builder.query<{ success: boolean; article: Article }, number>({
+      query: (id) => `/article/${id}`,
+      providesTags: ["Article"],
+    }),
+    getArticleByTitle: builder.query<
+      { success: boolean; articles: Article[] },
+      string
+    >({
+      query: (title) => `/article/search/${title}`,
+      providesTags: ["Article"],
+    }),
+    deleteArticle: builder.mutation<
+      { success: boolean; message: string },
+      number
+    >({
+      query: (id) => ({
+        url: `/article/${id}/delete`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: ["Article"],
+    }),
   }),
 });
 
-export const { useCreateArticleMutation } = articleApi;
+export const {
+  useCreateArticleMutation,
+  useUpdateArticleMutation,
+  useGetAllArticleQuery,
+  useGetArticleQuery,
+  useGetArticleByTitleQuery,
+  useDeleteArticleMutation,
+} = articleApi;
