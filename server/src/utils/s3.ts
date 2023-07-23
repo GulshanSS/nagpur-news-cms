1f2c5c1b7532627ef0ec7e9bf2649df8ba57bdac
GulshanSS <sahugulshan686@gmail.com>
2023-07-24 00:14:08 +0530

    Add: InvalidateCloudFront cache method and chnages getSignedUrlForMedia method

--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -1,11 +1,14 @@
 import {
   DeleteObjectCommand,
-  GetObjectCommand,
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
 import config from "../config";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
+import {
+  CloudFrontClient,
+  CreateInvalidationCommand,
+} from "@aws-sdk/client-cloudfront";
 
 const s3 = new S3Client({
   region: config.AWS_BUCKET_REGION,
@@ -15,13 +18,37 @@ const s3 = new S3Client({
   },
 });
 
-export const getSignedUrlForMedia = async (key: string) => {
-  const params = {
-    Bucket: config.AWS_BUCKET_NAME,
-    Key: key,
+const cloudFront = new CloudFrontClient({
+  region: config.AWS_BUCKET_REGION,
+  credentials: {
+    accessKeyId: config.AWS_ACCESS_KEY,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+export const invalidateCloudFrontCache = async (key: string) => {
+  const invalidateParams = {
+    DistributionId: config.CLOUDFRONT_DISTRIBUTION_ID,
+    InvalidationBatch: {
+      CallerReference: key,
+      Paths: {
+        Quantity: 1,
+        Items: ["/" + key],
+      },
+    },
   };
-  const command = new GetObjectCommand(params);
-  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+  const invalidationCommand = new CreateInvalidationCommand(invalidateParams);
+  await cloudFront.send(invalidationCommand);
+};
+
+export const getSignedUrlForMedia = async (key: string) => {
+  const url = await getSignedUrl({
+    url: `${config.CLOUDFRONT_URL}/${key}`,
+    dateLessThan: new Date(Date.now() + 1000 * 60 * 60 * 24).toString(),
+    privateKey: config.CLOUDFRONT_PRIVATE_KEY,
+    keyPairId: config.CLOUDFRONT_KEY_PAIR_ID,
+  });
   return url;
 };
 
