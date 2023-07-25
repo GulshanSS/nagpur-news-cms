d07f4c5fb1480d21b6e927801c0531a2f3308463
GulshanSS <sahugulshan686@gmail.com>
2023-07-25 20:26:31 +0530

    Add: NavItem Component

---
+++ client/src/components/NavItem.tsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+type Props = {
+  onClick?: () => void;
+  icon: JSX.Element;
+  text: string;
+  to: string;
+};
+
+const NavItem = ({ icon, text, to, onClick }: Props) => {
+  return (
+    <>
+      <li onClick={onClick}>
+        <NavLink
+          to={`/${to}`}
+          className={({ isActive }) =>
+            `flex items-center p-2 rounded-lg ${
+              isActive
+                ? "text-custom-800 bg-custom-100 border border-custom-600"
+                : " text-custom-800  hover:bg-custom-100"
+            }`
+          }
+        >
+          <span className="text-2xl">{icon}</span>
+          <span className="ml-3">{text}</span>
+        </NavLink>
+      </li>
+    </>
+  );
+};
+
+export default NavItem;
