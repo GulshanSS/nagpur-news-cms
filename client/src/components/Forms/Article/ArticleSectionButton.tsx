472bed96a47e2f8f7d3bf6b150dfddfb7b10590c
GulshanSS <sahugulshan686@gmail.com>
2023-07-25 20:32:07 +0530

    Fix: Removed icon and added label as param

--- client/src/components/Forms/Article/ArticleSectionButton.tsx
+++ client/src/components/Forms/Article/ArticleSectionButton.tsx
@@ -1,31 +1,17 @@
 type Props = {
   setModalCloseForm: Function;
+  label: string;
 };
 
-const ArticleSectionButton = ({ setModalCloseForm }: Props) => {
+const ArticleSectionButton = ({ setModalCloseForm, label }: Props) => {
   return (
     <>
       <button
         type="button"
-        className="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mb-2"
-        onClick={(e) => setModalCloseForm(true)}
+        className="text-custom-800 border border-custom-600 bg-custom-100 focus:ring-4 focus:outline-none focus:ring-custom-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+        onClick={() => setModalCloseForm(true)}
       >
-        <svg
-          className="w-4 h-4 mr-2 -ml-1 text-[#626890]"
-          xmlns="http://www.w3.org/2000/svg"
-          width="31"
-          height="31"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#000000"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <line x1="12" y1="5" x2="12" y2="19"></line>
-          <line x1="5" y1="12" x2="19" y2="12"></line>
-        </svg>
-        Add Section
+        {label}
       </button>
     </>
   );
