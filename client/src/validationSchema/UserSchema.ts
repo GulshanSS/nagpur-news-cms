60a1425d8796cf7250b12bf8ab5e7693967db2b9
GulshanSS <sahugulshan686@gmail.com>
2023-07-31 02:06:16 +0530

    Add: setAsAdmin field to validation schema

--- client/src/validationSchema/UserSchema.ts
+++ client/src/validationSchema/UserSchema.ts
@@ -3,6 +3,7 @@ import { TypeOf, z } from "zod";
 export const UserSchema = z.object({
   name: z.string().min(1, "User name is required"),
   email: z.string().email("Invalid email. Enter valid email"),
+  setAsAdmin: z.boolean().optional(),
   active: z.boolean().optional(),
 });
 
