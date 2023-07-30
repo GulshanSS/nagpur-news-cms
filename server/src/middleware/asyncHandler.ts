ed417f5da38df891838d2f30bc63b9251c0d1ba0
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 22:40:22 +0530

    Remove: Unwanted log

--- server/src/middleware/asyncHandler.ts
+++ server/src/middleware/asyncHandler.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from "express";
+import logger from "../utils/logger";
 
 const asyncHandler =
-  (fn: any) => (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
+  (fn: any) => (req: Request, res: Response, next: NextFunction) => {
+    logger.info(`${req.method} : ${req.baseUrl}${req.url}`);
+    return Promise.resolve(fn(req, res, next)).catch(next);
+  };
 
 export default asyncHandler;
