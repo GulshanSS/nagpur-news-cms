5ba632bfdb434e5c651b3ed195a9218fe158cffb
GulshanSS <sahugulshan686@gmail.com>
2023-07-31 02:01:43 +0530

    Fix: spinner color

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -7,7 +7,7 @@ const Spinner = () => {
       >
         <svg
           aria-hidden="true"
-          className="inline w-12 h-12 text-custom-50 animate-spin fill-custom-100"
+          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin fill-yellow-800"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
