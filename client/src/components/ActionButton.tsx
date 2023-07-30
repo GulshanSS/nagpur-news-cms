83799b40523085cb4ee202085d2482b50e9aba40
GulshanSS <sahugulshan686@gmail.com>
2023-07-31 01:59:17 +0530

    Add:  Button Type

--- client/src/components/ActionButton.tsx
+++ client/src/components/ActionButton.tsx
@@ -17,6 +17,7 @@ const ActionButton = ({ onClick, Icon, color }: Props) => {
     <>
       <button
         onClick={onClick}
+        type="button"
         className={`flex justify-center items-center text-xl w-8 h-8 rounded-full bg-custom-100 ease-in-out duration-300 ${colorVariants[color]}`}
       >
         {Icon}
