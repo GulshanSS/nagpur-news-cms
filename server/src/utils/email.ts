ecd216934b7cc7d21d01a029a11662d4459f1701
GulshanSS <sahugulshan686@gmail.com>
2023-07-31 15:06:04 +0530

    Fix: Email Templates

--- server/src/utils/email.ts
+++ server/src/utils/email.ts
@@ -15,21 +15,125 @@ const transporter = nodemailer.createTransport({
 
 export const OTPTemplate = (otp: string) => ({
   subject: "Verify Account: Nagpur News CMS",
-  html: `<p>OTP:<b>${otp}</b></p>`,
+  html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+  <html lang="en">
+  
+    <head></head>
+    <div id="__react-email-preview" style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Verify your email address<div>
+    </div>
+    </div>
+  
+    <body style="background-color:#ffffff;margin:0 auto;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif">
+      <table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%" style="max-width:600px;margin:0 auto">
+        <tr style="width:100%">
+          <td>
+            <h1 style="color:#1d1c1d;font-size:36px;font-weight:700;margin:30px 0;padding:0;line-height:42px">Verify your email address</h1>
+            <p style="font-size:20px;line-height:28px;margin:16px 0;margin-bottom:30px">Your OTP is below - enter it in your open browser window and we&#x27;ll help you get signed in.</p>
+            <table style="background:rgb(245, 244, 245);border-radius:4px;margin-right:50px;margin-bottom:30px;padding:43px 23px" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%">
+              <tbody>
+                <tr>
+                  <td>
+                    <p style="font-size:30px;line-height:24px;margin:16px 0;text-align:center;vertical-align:middle"><b>${otp}</b></p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p style="font-size:14px;line-height:24px;margin:16px 0;color:#000">If you didn&#x27;t request this email, there&#x27;s nothing to worry about - you can safely ignore it.</p>
+          </td>
+        </tr>
+      </table>
+    </body>
+  
+  </html>`,
 });
 
 export const AccountCreationTemplate = (email: string, password: string) => ({
   subject: "Welcome: Nagpur News CMS",
-  html: `<p>Please find below assigned credentials</p><br>
-    <p>Email: <b>${email}</b></p><br>
-    <p>Password: <b>${password}</b></p><br>`,
+  html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+  <html lang="en">
+  
+    <head></head>
+    <div id="__react-email-preview" style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Login Credentials<div></div>
+    </div>
+  
+    <body style="background-color:#fff;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Oxygen-Sans,Ubuntu,Cantarell,&quot;Helvetica Neue&quot;,sans-serif">
+      <table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%" style="max-width:37.5em">
+        <tr style="width:100%">
+          <td>
+            <table style="border:1px solid rgb(0,0,0, 0.1);border-radius:3px;overflow:hidden" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%">
+              <tbody>
+                <tr>
+                  <td>
+                    <table width="100%" style="padding:20px 40px;padding-bottom:0" align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0">
+                      <tbody style="width:100%">
+                        <tr style="width:100%">
+                          <td>
+                            <h1 style="font-size:32px;font-weight:bold;text-align:center">Hi,</h1>
+                            <h2 style="font-size:26px;font-weight:bold;text-align:center">Welcome to Nagpur News CMS !!!</h2>
+                            <p style="font-size:16px;line-height:24px;margin:16px 0"><b>Email: ${email}</p>
+                            <p style="font-size:16px;line-height:24px;margin:16px 0;margin-top:-5px"><b>Password: </b>${password}</p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <table width="100%" style="padding:20px 40px;padding-top:0" align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0">
+                      <tbody style="width:100%">
+                        <tr style="width:100%">
+                          <td colSpan="2" style="display:flex;justify-content:center;width:100%"><a href=${config.CMS_URL}/login target="_blank" style="background-color:#e00707;padding:0px 0px;border-radius:3px;color:#FFF;font-weight:bold;border:1px solid rgb(0,0,0, 0.1);cursor:pointer;line-height:100%;text-decoration:none;display:inline-block;max-width:100%"><span><!--[if mso]><i style="letter-spacing: undefinedpx;mso-font-width:-100%;mso-text-raise:0" hidden>&nbsp;</i><![endif]--></span><span style="background-color:#e00707;padding:12px 30px;border-radius:3px;color:#FFF;font-weight:bold;border:1px solid rgb(0,0,0, 0.1);cursor:pointer;max-width:100%;display:inline-block;line-height:120%;text-decoration:none;text-transform:none;mso-padding-alt:0px;mso-text-raise:0">Login</span><span><!--[if mso]><i style="letter-spacing: undefinedpx;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>
+  
+  </html>`,
 });
 
 export const SendResetPasswordLinkTemplate = (token: string) => ({
   subject: "Reset Password: Nagpur News CMS",
-  html: `<a href=${config.CMS_URL}/reset-password/?token=${encodeURIComponent(
+  html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+  <html lang="en">
+  
+    <head></head>
+    <div id="__react-email-preview" style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Nagpur News CMS reset your password<div>
+    </div>
+    </div>
+  
+    <body style="background-color:#f6f9fc;padding:10px 0">
+      <table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%" style="max-width:37.5em;background-color:#ffffff;border:1px solid #f0f0f0;padding:45px">
+        <tr style="width:100%">
+          <td>
+            <table align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%">
+              <tbody>
+                <tr>
+                  <td>
+                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">Hi,</p>
+                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">Someone recently requested a password change for your Nagpur News CMS account. If this was you, you can set a new password here:</p><a href=${
+                      config.CMS_URL
+                    }/reset-password/?token=${encodeURIComponent(
     token
-  )}><button>Reset Password</button></a>`,
+  )} target="_blank" style="background-color:#007ee6;border-radius:4px;color:#fff;font-family:&#x27;Open Sans&#x27;, &#x27;Helvetica Neue&#x27;, Arial;font-size:15px;text-decoration:none;text-align:center;display:inline-block;width:210px;padding:0px 0px;line-height:100%;max-width:100%"><span><!--[if mso]><i style="letter-spacing: undefinedpx;mso-font-width:-100%;mso-text-raise:0" hidden>&nbsp;</i><![endif]--></span><span style="background-color:#007ee6;border-radius:4px;color:#fff;font-family:&#x27;Open Sans&#x27;, &#x27;Helvetica Neue&#x27;, Arial;font-size:15px;text-decoration:none;text-align:center;display:inline-block;width:210px;padding:14px 7px;max-width:100%;line-height:120%;text-transform:none;mso-padding-alt:0px;mso-text-raise:0">Reset password</span><span><!--[if mso]><i style="letter-spacing: undefinedpx;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>
+                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">If you don&#x27;t want to change your password or didn&#x27;t request this, just ignore and delete this message.</p>
+                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:#404040">To keep your account secure, please don&#x27;t forward this email to anyone.</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>
+  
+  </html>`,
 });
 
 export const sendEmail = async (
