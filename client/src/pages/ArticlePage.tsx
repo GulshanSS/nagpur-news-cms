e5482247dd5cefa88b30f0db6c8788b1b13c25cf
GulshanSS <sahugulshan686@gmail.com>
2023-08-14 02:01:31 +0530

    Fix: minor fix

--- client/src/pages/ArticlePage.tsx
+++ client/src/pages/ArticlePage.tsx
@@ -1,37 +1,26 @@
 import { useState } from "react";
-import ArticleForm from "../components/Forms/Article/ArticleForm";
-import { CreateArticleSchema } from "../validationSchema/ArticleSchema";
 import PageNav from "../components/PageNav";
-import Modal from "../components/Modal";
 import AddResourceButton from "../components/AddResourceButton";
 import DisplayAllArticle from "../components/Article/DisplayAllArticle";
 
 const ArticlePage = () => {
-  const [modalCloseForm, setModalCloseForm] = useState<boolean>(false);
   const [searchQuery, setSearchQuery] = useState<string>("");
   return (
     <>
       <>
         <PageNav
-          setModalCloseForm={setModalCloseForm}
           searchQuery={searchQuery}
           setSearchQuery={setSearchQuery}
+          navigateToDifferentPage={true}
+          navigateTo="/article/create"
         />
         <div className="w-auto py-32">
           <DisplayAllArticle searchQuery={searchQuery} />
         </div>
-        <Modal
-          id="articleForm"
-          close={modalCloseForm}
-          setClose={setModalCloseForm}
-        >
-          <ArticleForm
-            buttonLabel="Publish"
-            actionButtonLabel="Save As Draft"
-            schema={CreateArticleSchema}
-          />
-        </Modal>
-        <AddResourceButton setModalCloseForm={setModalCloseForm} />
+        <AddResourceButton
+          navigateToDifferentPage={true}
+          navigateTo="/article/create"
+        />
       </>
     </>
   );
