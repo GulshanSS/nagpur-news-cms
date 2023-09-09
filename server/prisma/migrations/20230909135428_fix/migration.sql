5418e3400846aa3a606867ee70aa5b90ef330458
GulshanSS <sahugulshan686@gmail.com>
2023-09-09 20:27:23 +0530

    Add: slug field to the required tables

---
+++ server/prisma/migrations/20230909135428_fix/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "ArticleSection" ADD COLUMN     "slug" TEXT;
+
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "slug" TEXT;
+
+-- AlterTable
+ALTER TABLE "PromotionaryArticle" ADD COLUMN     "slug" TEXT;
+
+-- AlterTable
+ALTER TABLE "Tag" ADD COLUMN     "slug" TEXT;
---
+++ server/prisma/migrations/20230909135656_fix/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `slug` on the `ArticleSection` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ArticleSection" DROP COLUMN "slug";
