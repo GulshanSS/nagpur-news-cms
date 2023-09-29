820a3460c68f411c7290932d476c63d02b4c7ea0
GulshanSS <sahugulshan686@gmail.com>
2023-09-30 01:36:27 +0530

    Fix: Using ImageKit sign url method

--- server/src/public/category/category.controller.ts
+++ server/src/public/category/category.controller.ts
@@ -6,9 +6,9 @@ import {
   getCategoryBySlug,
 } from "./category.service";
 import { AppError, HttpCode } from "../../exceptions/AppError";
-import { getSignedUrlForMedia } from "../../utils/s3";
 import config from "../../config";
 import db from "../../utils/db.server";
+import { getSignedUrlIK } from "../../utils/imageKit";
 
 export const getAllCategoriesHandler = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -45,7 +45,7 @@ export const getAllCategoriesWithMinArticlesHandler = asyncHandler(
       for (const article of category.article) {
         if (article.media.length > 0) {
           for (const media of article.media) {
-            media.key = await getSignedUrlForMedia(media.key);
+            media.key = getSignedUrlIK(media.key);
           }
         }
       }
@@ -114,7 +114,7 @@ export const getCategoryBySlugHandler = asyncHandler(
     for (const article of category.article) {
       if (article.media.length > 0) {
         for (const media of article.media) {
-          media.key = await getSignedUrlForMedia(media.key);
+          media.key = getSignedUrlIK(media.key);
         }
       }
     }
